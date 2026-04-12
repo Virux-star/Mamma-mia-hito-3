fe68c495277d13db6718aceb0b58ad1b7834bc75
virux <jantonioascencio10@gmail10.com>
2026-04-12 13:26:21 -0400

    moficacion home

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,37 +1,33 @@
-import Header from "../componentes/header";
-import CardPizza from "../componentes/cardpizza";
-import napolitana from "../assets/Para-la-masa-de-pizza-napolitana-8-hrs-fermentacion-Web-1-500x500.jpg";
-import española from "../assets/images (1).jpg";
-import pepperoni from "../assets/pizza-con-salami-chorizo-beacon.jpg";
+import { pizzas } from "../componentes/pizzas";
+import CardPizza from "../componentes/CardPizza";
+import Header from "../componentes/Header";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Header />
 
-      <div style={{ display: "flex", gap: "20px", padding: "40px 20px", justifyContent: "center", alignItems: "center", flexWrap: "wrap" }}>
-        <CardPizza
-          name="Napolitana"
-          price={5950}
-          ingredients={["mozzarella", "tomates", "jamón", "orégano"]}
-          img={napolitana}
-        />
-
-        <CardPizza
-          name="Española"
-          price={6950}
-          ingredients={["mozzarella", "gorgonzola", "parmesano", "provolone"]}
-          img={española}
-        />
-
-        <CardPizza
-          name="Pepperoni"
-          price={6950}
-          ingredients={["mozzarella", "pepperoni", "orégano"]}
-          img={pepperoni}
-        />
+      <div
+        style={{
+          display: "flex",
+          gap: "20px",
+          padding: "40px 20px",
+          justifyContent: "center",
+          alignItems: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        {pizzas.map((pizza) => (
+          <CardPizza
+            key={pizza.id}
+            name={pizza.name}
+            price={pizza.price}
+            ingredients={pizza.ingredients}
+            img={pizza.img}
+          />
+        ))}
       </div>
-    </div>
+    </>
   );
 };
 
