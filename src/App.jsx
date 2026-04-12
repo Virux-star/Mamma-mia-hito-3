f5c1efb383b36fd7db52ae01eb5f500cfa2430b0
virux <jantonioascencio10@gmail10.com>
2026-04-12 13:47:46 -0400

    avance en en el carrito

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Footer from "./componentes/footer";
 import Home from "./views/home";
 //import Register from "./views/registro";
 //import Login from "./views/login";
+import Cart from "./componentes/carrito";
 
 function App() {
   return (
     <>
       <Navbar />
 
-      {<Home />}
+      {/* <Home /> */}
       {/* <Login /> */}
       {/* <Register /> */}
+      { <Cart /> }
 
       <Footer />
     </>
